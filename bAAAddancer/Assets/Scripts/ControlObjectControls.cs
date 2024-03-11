a57a6fc31943aa3ab21ae7b40bb8dc02ee63dca5
LouFou2 <parkipos@gmail.com>
2024-03-11 11:40:13 +1100

    Controller Script
    Controller moves position directly, not using time.deltatime*movespeed
    Movement is within a set range
    lerps the joystick input to the range
    clamps the movement to range

--- bAAAddancer/Assets/Scripts/ControlObjectControls.cs
+++ bAAAddancer/Assets/Scripts/ControlObjectControls.cs
@@ -40,7 +40,8 @@ public class ControlObjectControls : MonoBehaviour
         float rangedX = Mathf.Lerp(-x_Range, x_Range, (moveInput.x + 1f) / 2f);
         float rangedY = Mathf.Lerp(-y_Range, y_Range, (moveInput.y + 1f) / 2f);
 
-        controlObjects[0].transform.position += new Vector3(-rangedX, rangedY, 0f) * Time.deltaTime * moveSpeed;
+        //controlObjects[0].transform.position += new Vector3(-rangedX, rangedY, 0f) * Time.deltaTime * moveSpeed;
+        controlObjects[0].transform.position = new Vector3(-rangedX + initialPosition.x, rangedY + initialPosition.y, 0f);
 
         Vector3 currentPosition = controlObjects[0].transform.position;
         float newX = Mathf.Clamp(currentPosition.x, initialPosition.x - x_Range, initialPosition.x + x_Range);
