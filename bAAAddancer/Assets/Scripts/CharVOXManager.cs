e86775a173e0a10fdd0058f368a995055f63c538
LouFou2 <parkipos@gmail.com>
2024-06-13 16:49:48 +1000

    character audio sources and VOX manager
    each character has a script to manage voices
    added audio source to each character
    the plan is to call the methods for which voice to use, as well as which character to use,
    depending on dialogue data instructions

--- bAAAddancer/Assets/Scripts/CharVOXManager.cs
+++ bAAAddancer/Assets/Scripts/CharVOXManager.cs
@@ -4,17 +4,41 @@ using UnityEngine;
 
 public class CharVOXManager : MonoBehaviour
 {
-    /// <summary>
-    /// Will use this script to organise and manage voice clips for each character
-    /// </summary>
-    [System.Serializable]
-    public class VoxUnit 
+    [SerializeField] private AudioSource voxAudioSource;
+    [SerializeField] private AudioClip casualVoxClip;
+    [SerializeField] private AudioClip confusedVoxClip;
+    [SerializeField] private AudioClip happyVoxClip;
+    [SerializeField] private AudioClip madVoxClip;
+    [SerializeField] private AudioClip pleadingVoxClip;
+    [SerializeField] private AudioClip worriedVoxClip;
+    public void PlayCasualVox() 
     {
-        public DialogueData.SpeakingCharacter speakingCharacter;
-        public VoxExpression expression;
-        public AudioClip voxClip;
+        voxAudioSource.clip = casualVoxClip;
+        voxAudioSource.Play();
     }
-    public enum VoxExpression { Casual, Excited, Worried, Shady, Mad }
-
-    public VoxUnit[] voxUnits;
+    public void PlayConfusedVox() 
+    {
+        voxAudioSource.clip = confusedVoxClip;
+        voxAudioSource.Play();
+    }
+    public void PlayHappyVox() 
+    {
+        voxAudioSource.clip = happyVoxClip;
+        voxAudioSource.Play();
+    }
+    public void PlayMadVox() 
+    {
+        voxAudioSource.clip = madVoxClip;
+        voxAudioSource.Play();
+    }
+    public void PlayPleadingVox() 
+    {
+        voxAudioSource.clip = pleadingVoxClip;
+        voxAudioSource.Play();
+    }
+    public void PlayWorriedVox() 
+    {
+        voxAudioSource.clip = worriedVoxClip;
+        voxAudioSource.Play();
+    }
 }
