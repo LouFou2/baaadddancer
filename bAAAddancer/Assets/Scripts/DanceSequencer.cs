1cbeae72946ebf90aa665b9b4d602dc29b8189f0
LouFou2 <parkipos@gmail.com>
2024-06-15 18:21:42 +1000

    fixed script that only "records" if left stick is moving....

--- bAAAddancer/Assets/Scripts/DanceSequencer.cs
+++ bAAAddancer/Assets/Scripts/DanceSequencer.cs
@@ -59,21 +59,13 @@ public class DanceSequencer : MonoBehaviour
     void OnBeatTriggerHandler() // Method to handle beat trigger event
     {
         // === CHECKING INPUT === //
-        //bool record = playerControls.DanceControls.Record.IsPressed(); //***currently removed the record flag
-        bool record = false;
-
-        Vector2 inputVector2 = playerControls.DanceControls.MoveL.ReadValue<Vector2>();
-        Vector2 inputVector2b = playerControls.DanceControls.MoveR.ReadValue<Vector2>();
-
-        if (inputVector2.magnitude > 0.01f) record = true;
 
         for (int i = 0; i < objControlScripts.Length; i++)
         {
-            //if (objControlScripts[i].isActive && record)
             if (objControlScripts[i].isActive && objControlScripts[i].isRecording)
             {
                 objControlScripts[i].useRecordedPositions = false;
-                // Record the position + rotation for the current GameObject for the current beat
+                // Record the position for the current GameObject for the current beat
                 recordingDataObjects[i].recordedPositions[beatCount] = objControlScripts[i].GetPositionToRecord();
                 //recordingDataObjects[i].recordedRotations[beatCount] = objControlScripts[i].GetRotationToRecord();
             }
