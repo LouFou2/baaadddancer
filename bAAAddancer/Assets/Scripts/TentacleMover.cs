e8862aabce9c5092be8cf12b64bbcda4bca103da
LouFou2 <parkipos@gmail.com>
2025-05-17 23:44:26 +1000

    TentacleTinkering

--- bAAAddancer/Assets/Scripts/TentacleMover.cs
+++ bAAAddancer/Assets/Scripts/TentacleMover.cs
@@ -11,6 +11,9 @@ public class TentacleMover : MonoBehaviour
     private Vector3[] tentaclesPosL = new Vector3[5];
     private Vector3[] tentaclesPosR = new Vector3[5];
 
+    [SerializeField] private float scaleMovementX = 1.5f;
+    [SerializeField] private float scaleMovementY = 0.5f;
+
     private Vector3 mouthPosOffset;
 
     void Start()
@@ -27,7 +30,7 @@ public class TentacleMover : MonoBehaviour
     void Update()
     {
         Vector2 mouthPos = demonFaceScreenScript.GetMouthCenter();
-        Vector3 mouthPos3D = new Vector3(-mouthPos.x, mouthPos.y, 0); //***NOTE we invert the x here
+        Vector3 mouthPos3D = new Vector3(-mouthPos.x * scaleMovementX, mouthPos.y * scaleMovementY, 0); //***NOTE we invert the x here
 
         Vector3[] updatedTentaclePosL = new Vector3[5];
         Vector3[] updatedTentaclePosR = new Vector3[5];
